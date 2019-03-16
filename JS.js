5408f1066d99b406b40ee471a231f379c286faa8
SergiySobchuk <Sergiy.Sobchuk@gmail.com>
2019-03-16 18:49:13 +0200

    task_7 done

--- JS.js
+++ JS.js
@@ -118,7 +118,10 @@ function minMax(arr) {
 }
 minMax([1, 4, 8, 2, 20]) // { max: 20, min: 1 }
 
-// *******************task_7 (minMax)*****************
-console.log("********************task_7 (minMax)********************");
+// *******************task_8 (average)*****************
+console.log("********************task_7 (average)********************");
 
+function average(arr) {
 
+}
+average([1,4,2]) // 2.33
